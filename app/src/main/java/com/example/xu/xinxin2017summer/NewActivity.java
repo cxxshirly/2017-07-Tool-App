a6fcc2520ec421a9908110f5ba4b20c61902049f
cxxshirly <shirlypenguin@gmail.com>
2017-06-05 22:53:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/xu/xinxin2017summer/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.xu.xinxin2017summer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chen on 6/5/17.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
